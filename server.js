24ed879bd7610c4b1bbfbf077ebfca1bf25951f0
mehdicgi <gallardc@FR-D2565507.groupinfra.com>
2015-12-07 11:18:33 +0100

    added 3
    added 3

--- server.js
+++ server.js
@@ -129,15 +129,15 @@ var SampleApp = function()
             res.send('1');
         };
 
-        self.routes['/authorized'] = function(req, res) {
+        self.routes['/authorized3'] = function(req, res) {
              var forwardedIpsStr = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
            if (forwardedIpsStr) {
                 if (forwardedIpsStr == '194.3.185.38') {
-                     res.send('your are welcome'); 
+                     res.send('your are welcome T2'); 
                      res.end();
                     
                 }else{
-                    res.send('your ip '+forwardedIpsStr+' is not allowed T1'); 
+                    res.send('your ip '+forwardedIpsStr+' is not allowed T2'); 
                 
                   res.end();
                   
